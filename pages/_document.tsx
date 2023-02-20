8f7abed73751e0d6a064f1f54bf5150a460d9cf5
Abhisek Padhi <avicool000@gmail.com>
2023-02-20 10:33:36 +0530

    updated app version and added to html head

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,7 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
+                    <meta name="appVersion" content="0.2.0" />
                 </Head>
                 <body>
                     <Main />
